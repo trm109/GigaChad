2d869d6aac957dd5c8cabe0dc357c01687c49bac
Theo <trm109@case.edu>
2022-03-30 19:35:41 -0400

    Bye health component, hello player health.

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealthComponent : MonoBehaviour
+public class PlayerHealth : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,5 +14,8 @@ public class HealthComponent : MonoBehaviour
     void Update()
     {
         
+    }
+    public void TakeDamage(float health){
+
     }
 }
