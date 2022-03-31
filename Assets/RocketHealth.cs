e0fb4eb9b79e60c15a8102dfc8fac6c1b1e433fa
jlk201 <JLK201@case.edu>
2022-03-30 20:07:46 -0400

    Rocket Health
    Added rocket and rocket health

---
+++ Assets/RocketHealth.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketHealth : MonoBehaviour
+{
+    private int health = 200;
+    private int maxhealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxhealth = health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void RocketDamage(int n)
+    {
+        health = -n;
+    }
+}
