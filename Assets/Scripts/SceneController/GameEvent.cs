b8d479fcac49bb05a8ea7f7153fc3b2faed59904
Xochitl Villalvazo <dxv135@case.edu>
2022-03-31 11:47:27 -0400

    Adding generic UI elements

---
+++ Assets/Scripts/SceneController/GameEvent.cs
@@ -0,0 +1,8 @@
+﻿public static class GameEvent {
+	public const string ENEMY_KILLED = "ENEMY_KILLED";
+	public const string PLAYER_HIT = "PLAYER_HIT";
+	public const string SCORE_CHANGE = "SCORE_CHANGE";
+	public const string AIRSHIP_HIT = "AIRSHIP_HIT";
+	public const string WEAPON_CHANGE = "WEAPON_CHANGE";
+	public const string UPGRADE_ATTACK = "UPGRADE_ATTACK";
+}
