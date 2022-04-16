7ba442ac5ab91f04ad55527559b42cf39cef10d3
jlk201 <JLK201@case.edu>
2022-04-16 17:27:13 -0400

    Adding framework for audio system

---
+++ Assets/AttackAudio.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackAudio : MonoBehaviour
+{
+    private int counter;
+    public void setCounter(int n) { counter = n; }
+
+    public AudioClip fist;
+    public AudioClip brassknuckles;
+    public AudioClip sword;
+    public AudioClip morningstar;
+    public AudioClip axe;
+
+    private AudioSource a;
+
+    void Start()
+    {
+        a = GetComponent<AudioSource>();
+        counter = 1;
+    }
+
+    public void play()
+    {
+        switch (counter)
+        {
+            case 1:
+                a.PlayOneShot(fist, 1.0f);
+                break;
+            case 2:
+                a.PlayOneShot(brassknuckles, 1.0f);
+                break;
+            case 3:
+                a.PlayOneShot(sword, 1.0f);
+                break;
+            case 4:
+                a.PlayOneShot(morningstar, 1.0f);
+                break;
+            case 5:
+                a.PlayOneShot(fist, 1.0f);
+                break;
+            default:
+                break;
+        }
+
+    }
+}
