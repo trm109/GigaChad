db6be1555d1dfce116586f01d0ef6b39f2b6b513
Theo <trm109@case.edu>
2022-04-18 18:23:37 -0400

    Score Added

--- Assets/Scripts/SceneController/ScoreController.cs
+++ Assets/Scripts/SceneController/ScoreController.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public static class ScoreController : MonoBehaviour
+public class ScoreController : MonoBehaviour
 {
     public static Text scoreText;
     public static float score = 0;
     public static float scoreMult = 1.0f;
     // Start is called before the first frame update
+    private void Start() {
+        ScoreController.InitializeScore();
+    }
     static void InitializeScore(){
-        scoreText = GameObject.FindObjectWithTag("Score").GetComponent<scoreText>();
+        scoreText = GameObject.FindGameObjectWithTag("Score").GetComponent<Text>();
+        AllignScore();
     }
     public static void IncreaseScore(float amnt){
         score += amnt * scoreMult;
+        AllignScore();
+    }
+    public static void AllignScore(){
+        scoreText.text = score.ToString();
     }
 }
