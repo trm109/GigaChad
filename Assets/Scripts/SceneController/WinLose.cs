d7fdfe21f6a4cf7f11a7685389fc8e27ed0361e6
Xochitl Villalvazo <dxv135@case.edu>
2022-04-18 20:02:19 -0400

    Added score saving functionality

--- Assets/Scripts/SceneController/WinLose.cs
+++ Assets/Scripts/SceneController/WinLose.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class WinLose : MonoBehaviour
 {
+    [SerializeField] private ScoreController sc;    //reference to player score
+
     public void Win(){
         //Print Score
         //Add score to leaderboard
         //Show Play Again/Quit buttons.
+        SaveScore();
+        Application.LoadLevel("GameOverScreen");
     }
     public void Lose(){
         //Show sad message
         //dunno really
+        SaveScore();
+        Application.LoadLevel("GameOverScreen");
+    }
+    public void SaveScore()
+    {
+        PlayerPrefs.SetFloat("score", sc.GetScore());
     }
 }
